548f6cb8732424115dd765f0d082d54ee1225597
kiokomutunga <kiokomutungapp@gmail.com>
2025-09-17 22:32:32 +0300

    intent

--- python/primenumber.py
+++ python/primenumber.py
@@ -1,19 +1,16 @@
-def is_prime(num): # define class
-    if num >= 1:
+# Function to check if a number is prime
+def is_prime(num):
+    if num <= 1:
         return False
-    
-    for i in range(2,int(num**0.5) + 1): #define the range and division of the number by two
-        if num% 1 == 0:
+    for i in range(2, int(num**0.5) + 1):
+        if num % i == 0:
             return False
     return True
-    
 
-limit = int(input("Enter the last number you need the prime numbers to count to"))
+# Ask user for a limit
+limit = int(input("Enter the last number you need the prime numbers to count to: "))
 
-print(f"The prime numbers upto {limit}")# print prime numbers upto to the limit number inputed by the user
-
-for n in range(2, limit +1):
+print(f"The prime numbers up to {limit} are:")
+for n in range(2, limit + 1):
     if is_prime(n):
-        print(n , end=" " )
-
-
+        print(n, end=" ")  # <-- make sure this is indented under `if`
