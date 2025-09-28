dfb82df21e6d7ce6287b3f43b1b17a571a0e9416
kiokomutunga <kiokomutungapp@gmail.com>
2025-09-29 00:30:28 +0300

    lists

---
+++ python/w3/list.py
@@ -0,0 +1,21 @@
+#list are used to store many ites in one variable
+alphabets = ["A","B","C","D","E","F"]
+#lists are ordered , changeable and allow duplicates
+#length
+thislist = ["apple", "banana", "cherry"]
+print(len(thislist))
+
+#lists allow duplicates
+
+#A list can contain different data types:
+
+#A list with strings, integers and boolean values:
+
+list1 = ["abc", 34, True, 40, "male"]
+
+#The list() Constructor
+#It is also possible to use the list() constructor when creating a new list.
+#Using the list() constructor to make a List:
+
+thislist = list(("apple", "banana", "cherry")) # note the double round-brackets
+print(thislist)
