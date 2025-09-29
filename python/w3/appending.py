fbdde7c2167bdc247cc6bea8062fa1f7766a23d9
kiokomutunga <kiokomutungapp@gmail.com>
2025-09-29 20:49:20 +0300

    fhuhe

---
+++ python/w3/appending.py
@@ -0,0 +1,22 @@
+fruits = ["apple", "banana", "cherry", "kiwi", "mango"]
+newlist = []
+
+for x in fruits:
+  if "a" in x:
+    newlist.append(x)
+
+print(newlist)
+
+fruits = ["apple", "banana", "cherry", "kiwi", "mango"]
+
+newlist = [x for x in fruits if "a" in x]
+
+print(newlist)
+
+def myfunc(n):
+  return abs(n - 50)
+
+thislist = [100, 50, 65, 82, 23]
+thislist.sort(key = myfunc)
+print(thislist)
+
